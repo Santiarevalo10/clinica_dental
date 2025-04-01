8a5a74dd07f38f4c63861351b2f93cf15e8bf102
Santiarevalo10 <93667124+Santiarevalo10@users.noreply.github.com>
2025-03-31 19:42:54 -0500

    Update conexion.php

--- Conexion/conexion.php
+++ Conexion/conexion.php
@@ -1,13 +1,16 @@
 <?php
-// Archivo: ./Conexion/conexion.php
 $host = "clinica-dental-server.mysql.database.azure.com";
 $db = "clinica-dental";
 $user = "xbppwhznfp";
 $pass = "9k\$WTVDlyK\$gXdHL";
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$db;charset=utf8;sslmode=require",
+        $user,
+        $pass,
+        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+    );
 } catch (PDOException $e) {
     die("Error de conexión: " . $e->getMessage());
 }
